0e93d6192ce36d7d6b3fbb80ec092d2b7c51ed8f
ks12b0000 <ks12b0000@gmail.com>
2022-05-05 19:07:31 +0900

    Coding Test 2022-05-05

--- src/Doit알고리즘/IntervalSumSc.java
+++ src/Doit알고리즘/IntervalSumSc.java
@@ -4,28 +4,49 @@ import java.util.Scanner;
 
 public class IntervalSumSc {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		Scanner scan = new Scanner(System.in);
 		
-		int A = scan.nextInt();		//5입력
-		int B = scan.nextInt();		//3입력	
+		int A = scan.nextInt();			//5
+		int B = scan.nextInt();			//3
 		
-		
-		int[] S = new int[A+1];		//A에 1을 더해준 배열을 만듦
-									//문제에선 첫번째자리수를 0으로 읽는게 아니라 1부터 시작이라
-		
-		for(int i = 1; i <= A; i++) {	//A만큼 반복해서 i까지의 누적합을 구함
-			
-			S[i] = S[i-1] + scan.nextInt();
+		int[] arr = new int[A+1];		//arr[6]   사람이 입력할땐 0번째 자리부터가 아니라 1번째 자리부터 입력하는걸로 인식하기 때문에 
+										//		   배열에 1을 더해주어서 배열 6개를 만든 후 0~5번째 자리 값을 만들어줌.
+										//		  근데 우리는 1~5번째 자리값에만 입력해야 되므로 0번째는 입력을 넣어주지 않고 0으로만듦 
+		for(int i = 1; i <= A; i++) {						//		for(int i = 0; i < A; i++) {															
+			arr[i] = arr[i-1] + scan.nextInt();				//			arr[i+1] = arr[i] + scan.nextInt();
+			/*												//		}
+			 * arr[0] = 0;
+			 * arr[1] = arr[0] + 5   = 5
+			 * arr[2] = arr[1] + 4   = 9
+			 * arr[3] = arr[2] + 3   = 12
+			 * arr[4] = arr[3] + 2   = 14
+			 * arr[5] = arr[4] + 1   = 15
+			 */
 		}
 		for(int i = 0; i < B; i++) {
 			int C = scan.nextInt();
-			int J = scan.nextInt();
-			//C,J사이의 구간 합은 S[j] - S[C-1]과 같음
-			System.out.println(S[J] - S[C-1]);
+			int D = scan.nextInt();
+			
+			System.out.println(arr[D] - arr[C-1]);
+			/*
+			 *  C = 1  D = 3
+			 *  arr[1] ~ arr[3] 구간의 합 = arr[3] - arr[0] = 12
+			 * 								12      0
+			 *  C = 2  D = 4
+			 *  arr[2] ~ arr[4] 구간의 합 = arr[4] - arr[1] = 9
+			 *  							14      5
+			 *  C = 5  D = 5
+			 *  arr[5] ~ arr[5] 구간의 합 = arr[5] - arr[4] = 1
+			 *  							15 	    14
+			 */
 		}
 		
+		
+		
 		scan.close();
+		
 	}
 
 }
+
